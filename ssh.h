0437f9d5149afbb7c5431b57d7abb9b5c22139e3
Chris West (Faux) <git@goeswhere.com>
1999-11-17 14:00:45 +0000

    normalising ~0.48

--- ssh.h
+++ ssh.h
@@ -1,19 +1,18 @@
 #include <string.h>
 
-#define SSH_CIPHER_IDEA		1
-#define SSH_CIPHER_DES		2
-#define SSH_CIPHER_3DES		3
-#define SSH_CIPHER_BLOWFISH	6
+#define SSH_CIPHER_IDEA 1
+#define SSH_CIPHER_DES 2
+#define SSH_CIPHER_3DES 3
+#define SSH_CIPHER_BLOWFISH 6
 
 struct RSAKey {
-    int bits;
-    int bytes;
-    void *modulus;
-    void *exponent;
+  int bits;
+  int bytes;
+  void *modulus;
+  void *exponent;
 };
 
-int makekey(unsigned char *data, struct RSAKey *result,
-	    unsigned char **keystr);
+int makekey(unsigned char *data, struct RSAKey *result, unsigned char **keystr);
 void rsaencrypt(unsigned char *data, int length, struct RSAKey *key);
 int rsastr_len(struct RSAKey *key);
 void rsastr_fmt(char *str, struct RSAKey *key);
@@ -24,20 +23,21 @@ typedef unsigned int uint32;
 unsigned long crc32(const unsigned char *s, unsigned int len);
 
 struct MD5Context {
-        uint32 buf[4];
-        uint32 bits[2];
-        unsigned char in[64];
+  uint32 buf[4];
+  uint32 bits[2];
+  unsigned char in[64];
 };
 
 void MD5Init(struct MD5Context *context);
-void MD5Update(struct MD5Context *context, unsigned char const *buf,
+void MD5Update(struct MD5Context *context,
+               unsigned char const *buf,
                unsigned len);
 void MD5Final(unsigned char digest[16], struct MD5Context *context);
 
 struct ssh_cipher {
-    void (*sesskey)(unsigned char *key);
-    void (*encrypt)(unsigned char *blk, int len);
-    void (*decrypt)(unsigned char *blk, int len);
+  void (*sesskey)(unsigned char *key);
+  void (*encrypt)(unsigned char *blk, int len);
+  void (*decrypt)(unsigned char *blk, int len);
 };
 
 void SHATransform(word32 *digest, word32 *data);
@@ -45,4 +45,4 @@ void SHATransform(word32 *digest, word32 *data);
 int random_byte(void);
 void random_add_noise(void *noise, int length);
 
-void logevent (char *);
+void logevent(char *);
