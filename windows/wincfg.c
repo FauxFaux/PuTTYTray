648e4ea636c52c311b176bb94d0013e278501971
Chris West (Faux) <git@goeswhere.com>
2011-02-14 21:25:11 +0000

    PuTTy Tray 0.60 v3: Reconnect

--- windows/wincfg.c
+++ windows/wincfg.c
@@ -352,6 +352,13 @@ void win_setup_config_box(struct controlbox *b, HWND *hwndp, int has_help,
 	s = ctrl_getset(b, "Window", "main", "Window transparency options");
     ctrl_editbox(s, "Opacity (50-255)", 't', 30, HELPCTX(no_help), dlg_stdeditbox_handler, I(offsetof(Config,transparency)), I(-1));
 
+	/*
+	 * HACK: PuttyTray / Reconnect
+	 */
+	s = ctrl_getset(b, "Connection", "reconnect", "Reconnect options");
+	ctrl_checkbox(s, "Attempt to reconnect on system wakeup", 'w', HELPCTX(no_help), dlg_stdcheckbox_handler, I(offsetof(Config,wakeup_reconnect)));
+	ctrl_checkbox(s, "Attempt to reconnect on connection failure", 'f', HELPCTX(no_help), dlg_stdcheckbox_handler, I(offsetof(Config,failure_reconnect)));
+
     /*
      * Windows supports a local-command proxy. This also means we
      * must adjust the text on the `Telnet command' control.
