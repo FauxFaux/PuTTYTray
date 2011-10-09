dda1c990507aefbe0affcbfa942dbf87d2d34885
Chris West (Faux) <git@goeswhere.com>
2011-10-09 15:01:29 +0100

    #5: Build scripts get the version numbers right

--- windows/build-debug.bat
+++ windows/build-debug.bat
@@ -1,2 +1,3 @@
-@rem findstr warning | 
-nmake /F Makefile.vc "XFLAGS=/DDEBUG /Zi /Od /D_CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES /W1 /RTC1 /GS" XLFLAGS=/debug putty.exe 2>&1 | findstr /v overriding | findstr /v _CRT_SECURE_NO_WARNINGS
+@for /f "usebackq delims=" %%i in (`git describe`) do set desc=%%i
+for /f "tokens=2,3 delims=tv-" %%i in ("%desc%") do set trayversion=%%i & set traypatch=%%j
+nmake /F Makefile.vc "VER=/DSNAPSHOT= /DSVN_REV=%desc% /DSVN_REV1=%trayversion% /DSVN_REV2=%traypatch%" "XFLAGS=/DDEBUG /Zi /Od /D_CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES /W1 /RTC1 /GS" XLFLAGS=/debug putty.exe 2>&1 | findstr /v overriding | findstr /v _CRT_SECURE_NO_WARNINGS
