ede1712ca5c30de76d3e22e82eef92f8df53fb6f
Chris West (Faux) <git@goeswhere.com>
2011-10-09 15:01:29 +0100

    #5: Build scripts get the version numbers right

--- windows/build-release.bat
+++ windows/build-release.bat
@@ -1,1 +1,6 @@
-nmake /F Makefile.vc "XFLAGS=/Zi /D_CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES /GL /GS /Gy" "XLFLAGS=/debug /release /dynamicbase /nxcompat /ltcg" LFLAGS=/incremental:no putty.exe
+set trayversion=
+for /f "tokens=2,3 delims=-t" %%i in ("%1") do set trayversion=%%i
+if %trayversion%.==. echo Usage: %0 p0.61-t004 & goto end
+
+nmake /F Makefile.vc "VER=/DRELEASE=%1 /DSVN_REV1=%trayversion%" "XFLAGS=/Zi /D_CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES /GL /GS /Gy" "XLFLAGS=/debug /release /dynamicbase /nxcompat /ltcg" LFLAGS=/incremental:no putty.exe
+:end
