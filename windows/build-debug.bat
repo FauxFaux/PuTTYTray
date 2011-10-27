437aafa8e85ef008165365678b6b0a511ea49c79
Chris West (Faux) <git@goeswhere.com>
2011-10-27 23:09:24 +0100

    Allow first commit to be a debug build

--- windows/build-debug.bat
+++ windows/build-debug.bat
@@ -1,3 +1,4 @@
 @for /f "usebackq delims=" %%i in (`git describe`) do set desc=%%i
 for /f "tokens=2,3 delims=tv-" %%i in ("%desc%") do set trayversion=%%i & set traypatch=%%j
+if "%traypatch%" == "" set traypatch=9001
 nmake /F Makefile.vc "VER=/DSNAPSHOT= /DSVN_REV=%desc% /DSVN_REV1=%trayversion% /DSVN_REV2=%traypatch%" "XFLAGS=/DDEBUG /Zi /Od /D_CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES /W1 /RTC1 /GS" XLFLAGS=/debug putty.exe 2>&1 | findstr /v overriding | findstr /v _CRT_SECURE_NO_WARNINGS
