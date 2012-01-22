1490d72e5136234cd5e429d8ee5588510c960dd8
Chris West (Faux) <git@goeswhere.com>
2012-01-22 17:56:26 +0000

    build-release signs with signtool

--- windows/build-release.bat
+++ windows/build-release.bat
@@ -4,3 +4,4 @@ if %trayversion%.==. echo Usage: %0 p0.61-t004 & goto end
 
 nmake /F Makefile.vc "VER=/DRELEASE=%1 /DSVN_REV1=%trayversion%" "XFLAGS=/Zi /D_CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES /GL /GS /Gy" "XLFLAGS=/debug /release /dynamicbase /nxcompat /ltcg" LFLAGS=/incremental:no putty.exe
 :end
+signtool sign /a putty.exe
