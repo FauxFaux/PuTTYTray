f4683199e2b1d56664716aa8cae8effafa54c9ee
Chris West (Faux) <git@goeswhere.com>
2012-03-08 18:07:05 +0000

    gh-20: Never smush our backup of the icon, and always reset it even if we're not minimised at the time

--- windows/window.c
+++ windows/window.c
@@ -5858,6 +5858,7 @@ static void flash_window(int mode)
 			 * HACK: PuttyTray
 			 * Reset trayicon
 			 */
+			puttyTray.hIcon = puttyTrayFlashIcon;
 			if (puttyTrayVisible) {
 				puttyTrayFlash = FALSE;
 				puttyTray.hIcon = puttyTrayFlashIcon;
@@ -5898,7 +5899,6 @@ static void flash_window(int mode)
 				if (!puttyTrayFlash) {
 					puttyTrayFlash = TRUE;
 
-					puttyTrayFlashIcon = puttyTray.hIcon;
 					puttyTray.hIcon	= NULL;
 					taskbar_addicon(conf_get_int(conf, CONF_win_name_always) ? window_name : icon_name, TRUE);
 				} else {
