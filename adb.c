e4a28fb4cef81b8ec16b17ac8197cfb8620805c3
YumeYao <yumeyao@gmail.com>
2012-05-06 02:18:35 +0800

    GH-38: Fix adb-putty to support multi-devices
    To connect a specific device, input device ID in host name
    To connect the default device, input a colon(:) in host name

--- adb.c
+++ adb.c
@@ -191,23 +191,25 @@ static const char *adb_init(void *frontend_handle, void **backend_handle,
 	}
     }
 
-	/* send initial data to adb server */
-#if _MSC_VER
-	sprintf_s
-#else
-	snprintf
-#endif
-	(sendhost, 512, 
-#if _MSC_VER	
-	"%04xhost:%s"
-#else	
-	"%04zxhost:%s"
-#endif
-	, strlen(host)+5, host);
-
-	sk_write(adb->s,sendhost,strlen(host)+9);
-	sk_flush(adb->s);
-	adb->state = 1;
+    /* send initial data to adb server */
+#define ADB_SHELL_DEFAULT_STR "0012" "host:transport-usb"
+#define ADB_SHELL_DEFAULT_STR_LEN (sizeof(ADB_SHELL_DEFAULT_STR)-1)
+#define ADB_SHELL_SERIAL_PREFIX "host:transport:"
+#define ADB_SHELL_SERIAL_PREFIX_LEN (sizeof(ADB_SHELL_SERIAL_PREFIX)-1)
+    do {
+	size_t len = strlen(host);
+	if (len == 0) {
+	    sk_write(adb->s, ADB_SHELL_DEFAULT_STR, ADB_SHELL_DEFAULT_STR_LEN);
+	} else {
+	    char sendbuf[512];
+#define ADB_SHELL_HOST_MAX_LEN (sizeof(sendbuf)-4-ADB_SHELL_SERIAL_PREFIX_LEN)
+	    if (len > ADB_SHELL_HOST_MAX_LEN)
+		len = ADB_SHELL_HOST_MAX_LEN;
+	    sprintf(sendbuf,"%04x" ADB_SHELL_SERIAL_PREFIX, len+ADB_SHELL_SERIAL_PREFIX_LEN);
+	    memcpy(sendbuf+4+ADB_SHELL_SERIAL_PREFIX_LEN, host, len);
+	    sk_write(adb->s,sendbuf,len+4+ADB_SHELL_SERIAL_PREFIX_LEN);
+	}
+    } while (0);
     return NULL;
 }
 
