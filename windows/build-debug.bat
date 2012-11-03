6a362fd60a9517ec76a2ddc2d57d7df1f3040ca1
Chris West (Faux) <git@goeswhere.com>
2012-11-03 23:21:45 +0000

    Move compiler flags into mkfiles.pl

--- windows/build-debug.bat
+++ windows/build-debug.bat
@@ -1,4 +1,9 @@
-@for /f "usebackq delims=" %%i in (`git describe`) do set desc=%%i
+for /f "usebackq delims=" %%i in (`git describe`) do set desc=%%i
 for /f "tokens=2,3 delims=tv-" %%i in ("%desc%") do set trayversion=%%i & set traypatch=%%j
 if "%traypatch%" == "" set traypatch=9001
-nmake /F Makefile.vc "VER=/DSNAPSHOT= /DSVN_REV=%desc% /DSVN_REV1=%trayversion% /DSVN_REV2=%traypatch%" "XFLAGS=/DDEBUG /Zi /Od /RTC1 /GS" XLFLAGS=/debug putty.exe 2>&1 | findstr /v "D9025 : overriding '/O1' with '/Od'"
+nmake /F Makefile.vc DEBUG=1 ^
+  "VER=/DSNAPSHOT= /DSVN_REV=%desc% /DSVN_REV1=%trayversion% /DSVN_REV2=%traypatch%" ^
+  putty.exe ^
+  pageant.exe ^
+  2>&1 ^
+  | findstr /v overriding
