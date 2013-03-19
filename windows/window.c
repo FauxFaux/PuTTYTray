b3005bf76d12eebbe4b7bdd9b9c52e1ce34eae51
Chris West (Faux) <git@goeswhere.com>
2013-03-19 20:08:40 +0000

    innocent changes to PuTTYGen

--- windows/window.c
+++ windows/window.c
@@ -1066,7 +1066,7 @@ int WINAPI WinMain(HINSTANCE inst, HINSTANCE prev, LPSTR cmdline, int show)
 /*
  * Clean up and exit.
  */
-void cleanup_exit(int code)
+static void cleanup_exit(int code)
 {
     /* HACK: PuttyTray 
      * Remove trayicon on close 
