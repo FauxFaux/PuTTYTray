6d80813ae5b83fe4bd8e99a4d55c32bfd3144f53
Chris West (Faux) <git@goeswhere.com>
2013-07-14 18:34:29 +0100

    whitespace cleanup for urlhack_launch_url

--- windows/urlhack_win.c
+++ windows/urlhack_win.c
@@ -12,86 +12,86 @@ static int starts_with(const wchar_t *thing, const wchar_t *prefix) {
 
 void urlhack_launch_url(const char* app, const wchar_t *url)
 {
-	wchar_t *u;
-	if (app) {
-            wchar_t app_w[MAX_STR];
-            size_t newlen;
-            mbstowcs_s(&newlen, app_w, MAX_STR, app, MAX_STR);
-	    ShellExecuteW(NULL, NULL, app_w, url, NULL, SW_SHOW);
+    wchar_t *u;
+    if (app) {
+        wchar_t app_w[MAX_STR];
+        size_t newlen;
+        mbstowcs_s(&newlen, app_w, MAX_STR, app, MAX_STR);
+	ShellExecuteW(NULL, NULL, app_w, url, NULL, SW_SHOW);
+	return;
+    }
+
+    if (!wcslen(browser_app)) {
+	#define SUFFIX L"\\shell\\open\\command"
+	wchar_t str[MAX_STR] = L"";
+	HKEY key;
+	DWORD dwValue = MAX_STR - sizeof(SUFFIX) - 1;
+
+	// first let the OS try...
+	if ((long)ShellExecuteW(NULL, NULL, url, NULL, NULL, SW_SHOWNORMAL) > 32) {
 	    return;
 	}
 
-	if (!wcslen(browser_app)) {
-		#define SUFFIX L"\\shell\\open\\command"
-		wchar_t str[MAX_STR] = L"";
-		HKEY key;
-		DWORD dwValue = MAX_STR - sizeof(SUFFIX) - 1;
-
-		// first let the OS try...
-		if ((long)ShellExecuteW(NULL, NULL, url, NULL, NULL, SW_SHOWNORMAL) > 32) {
-			return;
-		}
-
-		// Find out the default app
-		if (RegOpenKeyExW(HKEY_CURRENT_USER,
-                        L"Software\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\http\\UserChoice",
-                        0, KEY_READ, &key) == ERROR_SUCCESS) {
-		    if (RegQueryValueExW(key, L"Progid", NULL, NULL, (BYTE*)str, &dwValue) == ERROR_SUCCESS) {
-			wcscat(str, SUFFIX);
-		    }
-		    RegCloseKey(key);
-		}
-
-		if (RegOpenKeyExW(HKEY_CLASSES_ROOT, wcslen(str) ? str : L"HTTP\\shell\\open\\command",
-                    0, KEY_READ, &key) == ERROR_SUCCESS) {
-			dwValue = MAX_STR;
-			if (!RegQueryValueExW(key, NULL, NULL, NULL, (BYTE*)str, &dwValue) == ERROR_SUCCESS) {
-				RegCloseKey(key);
-				return;
-			}
-			RegCloseKey(key);
-			
-			// Drop all stuff from the path and leave only the executable and the path
-			if (str[0] == '"') {
-				wchar_t *p = wcschr(str, L'"');
-
-				if (NULL != p)
-					*p = 0;
-				wcscpy(browser_app, str+1);
-			}
-			else {
-				wchar_t *p = wcschr(str, L'"');
-				if (NULL != p)
-					*p = 0;
-				wcscpy(browser_app, str);
-			}
-		}
-		else {
-			MessageBox(NULL, "Could not find your default browser.", "PuTTY Tray Error", MB_OK | MB_ICONINFORMATION);
-		}
+	// Find out the default app
+	if (RegOpenKeyExW(HKEY_CURRENT_USER,
+                L"Software\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\http\\UserChoice",
+                0, KEY_READ, &key) == ERROR_SUCCESS) {
+	    if (RegQueryValueExW(key, L"Progid", NULL, NULL, (BYTE*)str, &dwValue) == ERROR_SUCCESS) {
+		wcscat(str, SUFFIX);
+	    }
+	    RegCloseKey(key);
 	}
 
-	u = snewn(wcslen(url) + 10, wchar_t);
-	wcscpy(u, url);
+	if (RegOpenKeyExW(HKEY_CLASSES_ROOT, wcslen(str) ? str : L"HTTP\\shell\\open\\command",
+            0, KEY_READ, &key) == ERROR_SUCCESS) {
+	    dwValue = MAX_STR;
+	    if (!RegQueryValueExW(key, NULL, NULL, NULL, (BYTE*)str, &dwValue) == ERROR_SUCCESS) {
+		RegCloseKey(key);
+		return;
+	    }
+	    RegCloseKey(key);
 
-	if (!starts_with(url, L"http://") && !starts_with(url, L"https://") &&
-	    !starts_with(url, L"ftp://") && !starts_with(url, L"ftps://")) {
-	    if (wcsstr(url, L"ftp.")) {
-		wcscpy(u, L"ftp://");
-		wcscat(u, url);
-	    } else {
-		wcscpy(u, L"http://");
-		wcscat(u, url);
+	    // Drop all stuff from the path and leave only the executable and the path
+	    if (str[0] == '"') {
+		wchar_t *p = wcschr(str, L'"');
+
+		if (NULL != p)
+		    *p = 0;
+		wcscpy(browser_app, str+1);
+	    }
+	    else {
+		wchar_t *p = wcschr(str, L'"');
+		if (NULL != p)
+		    *p = 0;
+		wcscpy(browser_app, str);
 	    }
 	}
+	else {
+	    MessageBox(NULL, "Could not find your default browser.", "PuTTY Tray Error", MB_OK | MB_ICONINFORMATION);
+	}
+    }
 
-	ShellExecuteW(NULL, NULL, browser_app, u, NULL, SW_SHOW);
-	free(u);
+    u = snewn(wcslen(url) + 10, wchar_t);
+    wcscpy(u, url);
+
+    if (!starts_with(url, L"http://") && !starts_with(url, L"https://") &&
+	!starts_with(url, L"ftp://") && !starts_with(url, L"ftps://")) {
+	if (wcsstr(url, L"ftp.")) {
+	    wcscpy(u, L"ftp://");
+	    wcscat(u, url);
+	} else {
+	    wcscpy(u, L"http://");
+	    wcscat(u, url);
+	}
+    }
+
+    ShellExecuteW(NULL, NULL, browser_app, u, NULL, SW_SHOW);
+    free(u);
 }
 
 int urlhack_is_ctrl_pressed()
 {
-	return HIWORD(GetAsyncKeyState(VK_CONTROL));
+    return HIWORD(GetAsyncKeyState(VK_CONTROL));
 }
 
 
